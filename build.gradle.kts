b672933c8e5eec4236739682eef327cd7e199589
criswil5421 <cristian.aguirre@upeu.edu.pe>
2020-10-16 10:32:50 -0500

    actualizacion

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,17 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    ext.kotlin_version = "1.3.72"
+    //ext.kotlin_version = "1.3.72"
     repositories {
         google()
         jcenter()
+        maven("https://plugins.gradle.org/m2/")
     }
     dependencies {
-        classpath "com.android.tools.build:gradle:4.0.1"
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
 
+        classpath(Dependencies.gradle)
+        classpath(Dependencies.kotlinGradle)
+        classpath(Dependencies.daggerHilt)
+        classpath(Dependencies.ktLint)
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
@@ -21,6 +24,6 @@ allprojects {
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class) {
+    delete (rootProject.buildDir)
 }
